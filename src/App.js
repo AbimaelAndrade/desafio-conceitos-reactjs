b3df72a1476bbd3996089d5ed337660aab542e75
abimaelandrade <abimaelc.andrade@gmail.com>
2020-04-16 23:12:53 -0300

    Desafio concluído

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+      handleAllRepositories();
+  }, []);
+
+  async function handleAllRepositories(){
+    const response = await api.get('/repositories');
+    const repositories = response.data;
+
+    setRepositories(repositories)
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post(`/repositories`, {title});
+    const repository = response.data;
+
+    setRepositories([...repositories, repository])
+    setTitle('');
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const response = repositories.filter(repository => repository.id !== id );
+
+    setRepositories(response)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <span className="box">{repository.title}</span>
+          <button className="btn btn-remove" onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <div className="add-repositor">
+        <form>
+          <input 
+            type="text" 
+            name="title"
+            className="input-field" 
+            value={title} 
+            placeholder="Informe um projeto"
+            onChange={e => setTitle(e.target.value)}
+          />
+        </form>
+        <button className="btn btn-default" onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
